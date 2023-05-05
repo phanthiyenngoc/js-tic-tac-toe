33387aa79452d1586e22c00a2de5bfeb36ca9bbc
phanthiyenngoc <tech.ngocphan@gmail.com>
2023-05-05 16:14:39 +0700

    add selectors

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+import { getCellElementList, getCurrentTurnElement, getCellElementAtIdx, getGameStatusElement } from "./selectors.js";
+
 /**
  * Global variables
  */
+
+console.log(getCellElementList());
+console.log(getCurrentTurnElement());
+console.log(getCellElementAtIdx(4));
+console.log(getGameStatusElement());
 let currentTurn = "cross";
 let isGameEnded = false;
 let cellValues = new Array(9).fill("");
